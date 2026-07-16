95bc13b4b5e8cd6b577f8cb6950c8d847165d3e0
sumanjeet0012 <sumanjeet0012@gmail.com>
2026-07-16 20:22:10 +0530

    feat: export commonly used symbols from __init__

--- multiaddr/__init__.py
+++ multiaddr/__init__.py
@@ -1,5 +1,71 @@
-from .multiaddr import Multiaddr  # NOQA
+from .exceptions import (
+    BinaryParseError,
+    ParseError,
+    ProtocolExistsError,
+    ProtocolLookupError,
+    ProtocolNotFoundError,
+    ProtocolRegistryLocked,
+    RecursionLimitError,
+    ResolutionError,
+    StringParseError,
+)
+from .multiaddr import Multiaddr
+from .protocols import (
+    P_DNS,
+    P_DNS4,
+    P_DNS6,
+    P_DNSADDR,
+    P_IP4,
+    P_IP6,
+    P_P2P,
+    P_TCP,
+    P_UDP,
+    PROTOCOLS,
+    REGISTRY,
+    Protocol,
+    protocol_with_code,
+    protocol_with_name,
+)
+from .utils import (
+    get_multiaddr_options,
+    get_network_addrs,
+    get_thin_waist_addresses,
+    is_link_local_ip,
+    is_wildcard,
+)
 
 __author__ = "Steven Buss"
 __email__ = "steven.buss@gmail.com"
 __version__ = "0.2.0"
+
+__all__ = [
+    "PROTOCOLS",
+    "P_DNS",
+    "P_DNS4",
+    "P_DNS6",
+    "P_DNSADDR",
+    "P_IP4",
+    "P_IP6",
+    "P_P2P",
+    "P_TCP",
+    "P_UDP",
+    "REGISTRY",
+    "BinaryParseError",
+    "Multiaddr",
+    "ParseError",
+    "Protocol",
+    "ProtocolExistsError",
+    "ProtocolLookupError",
+    "ProtocolNotFoundError",
+    "ProtocolRegistryLocked",
+    "RecursionLimitError",
+    "ResolutionError",
+    "StringParseError",
+    "get_multiaddr_options",
+    "get_network_addrs",
+    "get_thin_waist_addresses",
+    "is_link_local_ip",
+    "is_wildcard",
+    "protocol_with_code",
+    "protocol_with_name",
+]
---
+++ newsfragments/121.feature
@@ -0,0 +1,1 @@
+Export commonly used symbols directly from `multiaddr/__init__.py`.
